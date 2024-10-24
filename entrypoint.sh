d3c758f813b5d7d45deefef0d492264a99ce56ef
Adriaan <1079135+adriaandotcom@users.noreply.github.com>
2024-10-24 15:07:00 +0200

    Do not get words part of other words

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,26 +2,24 @@
 
 set -eu -o pipefail
 
-# Remove spaces from the comma separated string
+# Remove spaces from the comma-separated string
 QUERY_TERMS=$(echo "$INPUT_QUERY" | sed 's/, /,/g')
 
-# Convert comma separated string to pipe separated string
+# Convert comma-separated string to pipe-separated string
 EXCLUDE_FOLDERS=$(echo "$INPUT_EXCLUDE" | sed -E 's/, ?/|/g')
 
 # Set found to 0
 found=0
 
+# Split the query terms into an array outside the loop for efficiency
+IFS=',' read -ra terms <<< "$QUERY_TERMS"
+
 # Loop through all files in the repo
-for file in $(find . -type f | grep -vE "$EXCLUDE_FOLDERS"); do
-
-  # Split the query terms into an array
-  IFS=',' read -ra terms <<< "$QUERY_TERMS"
-
-  # Loop through the array
+find . -type f | grep -vE "$EXCLUDE_FOLDERS" | while read -r file; do
+  # Loop through the array of forbidden terms
   for term in "${terms[@]}"; do
-
-    # If the term is found in the file, set found to 1
-    if grep -q "$term" "$file"; then
+    # If the term is found as a whole word in the file, set found to 1
+    if grep -qw "$term" "$file"; then
       echo "Found '$term' in file '$file'"
       found=1
     fi
